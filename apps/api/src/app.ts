8a9a5a10e643c441e7917f7a5374bd3967f1e4c0
anandshukla15 <anandshukla1742@gmail.com>
2026-08-15 19:50:22 +0530

    updated app

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,20 +2,16 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
+import healthRoutes from "./routes/health.routes.js";
+
 const app = express();
 
 app.use(helmet());
+
 
 app.use(cors());
 
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    success: true,
-    service: "scale-gate-api",
-    status: "healthy"
-  });
-});
-
+app.use("/health", healthRoutes);
 export default app;
