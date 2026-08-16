c3541bc1eba5b961b4a017871e84792f2ee4e6c1
anandshukla15 <anandshukla1742@gmail.com>
2026-08-16 15:31:05 +0530

    user route

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import healthRoutes from "./routes/health.routes.js";
 import { requestLogger } from "./middleware/requestLogger.js";
 import companyRoutes from "./routes/company.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
@@ -19,4 +20,5 @@ app.use(requestLogger);
 
 app.use("/health", healthRoutes);
 app.use("/api/companies", companyRoutes);
+app.use("/api/users", userRoutes);
 export default app;
