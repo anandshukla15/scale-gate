052c4624ef78302cc65e2585b70c23ef495a0ebf
anandshukla15 <anandshukla1742@gmail.com>
2026-08-17 00:41:08 +0530

    added in server

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,9 +1,11 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
 import {connectDatabase} from "./config/database.js";
+const { connectRedis } = await import("./config/redis.js");
 
 const startServer = async () => {
   await connectDatabase();
+  await connectRedis();
 
   app.listen(env.PORT, () => {
     console.log(
